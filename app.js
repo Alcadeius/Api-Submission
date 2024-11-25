543a1c60049295dfd829571fc082c999e41a7efc
Alcadeius <andresentanu3@gmail.com>
2024-11-25 19:26:18 +0800

    add universal port

--- app.js
+++ app.js
@@ -18,6 +18,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
